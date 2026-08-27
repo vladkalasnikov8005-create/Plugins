df56ef980deda61fd76ca1c8e127680974344551
vladkalasnikov8005-create <vladkalasnikov8005@gmail.com>
2026-08-28 01:59:39 +0300

    Update build.gradle.kts

--- untitled/build.gradle.kts
+++ untitled/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java-library")
-    id("xyz.jpenilla.run-paper") version "3.1.0"
 }
 
 repositories {
@@ -9,22 +8,14 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(25)
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks {
-    runServer {
-        // Configure the Minecraft version for our task.
-        // This is the only required configuration besides applying the plugin.
-        // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("26.2")
-        jvmArgs("-Xms2G", "-Xmx2G")
-    }
-
     processResources {
         val props = mapOf("version" to version)
         filesMatching("paper-plugin.yml") {
